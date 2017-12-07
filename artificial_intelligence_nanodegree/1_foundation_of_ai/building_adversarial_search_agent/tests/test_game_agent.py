b7e21281215dbfd45209ac2ef2e69b5056b9f90c
mk <mkao006@gmail.com>
2017-12-07 17:51:43 +0800

    added basic test for minimax

--- artificial_intelligence_nanodegree/1_foundation_of_ai/building_adversarial_search_agent/tests/test_game_agent.py
+++ artificial_intelligence_nanodegree/1_foundation_of_ai/building_adversarial_search_agent/tests/test_game_agent.py
@@ -5,8 +5,9 @@ cases used by the project assistant are not public.
 
 import unittest
 
+import timeit
 import isolation
-import game_agent
+import game_agent as ga
 
 from importlib import reload
 
@@ -15,14 +16,29 @@ class IsolationTest(unittest.TestCase):
     """Unit tests for isolation agents"""
 
     def setUp(self):
-        reload(game_agent)
+        reload(ga)
         self.player1 = "Player1"
         self.player2 = "Player2"
         self.game = isolation.Board(self.player1, self.player2)
+        self.time_limit = 150
 
-    def test_example(self):
-        # TODO: All methods must start with "test_"
-        self.fail("Hello, World!")
+    def _time_millis(self):
+        return 1000 * timeit.default_timer()
+
+    def _time_left(self, move_start):
+        def time_left():
+            return self.time_limit - (self._time_millis() - move_start)
+        return time_left
+
+    def test_move_output(self):
+        player = ga.MinimaxPlayer(search_depth=3, score_fn=ga.custom_score)
+        move_start = self._time_millis()
+        move = player.get_move(self.game, self._time_left(move_start))
+        print(move)
+        self.assertIsInstance(move, tuple)
+
+    def test_minimax_output(self):
+        pass
 
 
 if __name__ == '__main__':
