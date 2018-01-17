7ffa806b294bbdcffefefd16a488cb9a9d0024a1
mk <mkao006@gmail.com>
2018-01-17 16:47:37 +1300

    implemented recogniser

--- artificial_intelligence_nanodegree/1_foundation_of_ai/asl_recogniser/my_recognizer.py
+++ artificial_intelligence_nanodegree/1_foundation_of_ai/asl_recogniser/my_recognizer.py
@@ -18,8 +18,19 @@ def recognize(models: dict, test_set: SinglesData):
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    probabilities = []
-    guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+
+    def scoring_wrapper(model, idx):
+        try:
+            return model.score(*test_set.get_item_Xlengths(idx))
+        except:
+            return float('-inf')
+
+    probabilities = [
+        {model_key: scoring_wrapper(model_value, word_idx)
+         for model_key, model_value in models.items()}
+        for word_idx, word in enumerate(test_set.wordlist)]
+
+    guesses = [max(score.keys(), key=lambda x:score[x])
+               for score in probabilities]
+
+    return probabilities, guesses
